2cf0ae288950bba0dd06f6a7cfd45976127afbbe
Julian Rüth <julian.rueth@fsfe.org>
2019-06-28 06:14:00 +0200

    fix project name in copyright headers

--- test/pyeantic/doc.py
+++ test/pyeantic/doc.py
@@ -2,23 +2,23 @@
 # -*- coding: utf-8 -*-
 
 ######################################################################
-#  This file is part of intervalxt.
+#  This file is part of pyeantic.
 #
 #        Copyright (C) 2019 Vincent Delecroix
 #        Copyright (C) 2019 Julian Rüth
 #
-#  intervalxt is free software: you can redistribute it and/or modify
+#  pyeantic is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or (at your
 #  option) any later version.
 #
-#  intervalxt is distributed in the hope that it will be useful,
+#  pyeantic is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with intervalxt. If not, see <https://www.gnu.org/licenses/>.
+#  along with pyeantic. If not, see <https://www.gnu.org/licenses/>.
 #####################################################################
 
 import sys
